eed4b3bb013c17cd73e18a54e63617b1ccbeed43
Himanshu Kaushik <himanshu.kaushik@sumologic.com>
2023-12-17 04:12:23 +0530

    test

--- pagerduty/schedule.tf
+++ pagerduty/schedule.tf
@@ -1,7 +1,11 @@
 resource "test" "test" {
 <<<<<<< HEAD
+<<<<<<< HEAD
   description = "test5"
 =======
   description = "test7"
 >>>>>>> 9797f48 (test7)
+=======
+  description = "test8"
+>>>>>>> 922441b (test)
   }
